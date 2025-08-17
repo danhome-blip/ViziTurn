7fb8dd1daa27afa1d1e1a555616728776150ed37
danhome-blip <danvrinceanu2003@yahoo.com>
2025-08-17 11:35:31 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,7 @@
+// pages/_app.js
 import '../styles/globals.css';
-import { useState, useEffect } from 'react';
-import { supabase } from '../lib/supabase';
+import React from 'react';
 
-function MyApp({ Component, pageProps }) {
-  const [user, setUser] = useState(null);
-
-  // Verifică sesiunea la încărcare
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setUser(session?.user || null);
-    });
-
-    // Ascultă schimbările de autentificare
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      (event, session) => {
-        setUser(session?.user || null);
-      }
-    );
-
-    return () => {
-      authListener.subscription.unsubscribe();
-    };
-  }, []);
-
-  // Pasi user-ul ca prop la toate paginile
-  return <Component {...pageProps} user={user} />;
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;

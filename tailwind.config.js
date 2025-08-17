f0f71377a056ff91d615be299712f3c26706e945
danhome-blip <danvrinceanu2003@yahoo.com>
2025-08-17 11:51:00 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-  theme: {
-    extend: {
-      colors: {
-        brand: { DEFAULT: '#0EA5E9', dark: '#0284C7' }
-      }
-    }
-  },
+  theme: { extend: {} },
   plugins: []
-}
+};
